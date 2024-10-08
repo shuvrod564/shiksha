edd94ed90bb7c5e44c28549e7d8db1a065f8f67b
shuvrod564 <shuvrod564@gmail.com>
2024-10-09 00:08:40 +0600

    remove unwanted files

--- includes/stylesheets.inc.php
+++ includes/stylesheets.inc.php
@@ -2,8 +2,7 @@
 <!-- CSS: All Vendors -->
 <link rel="stylesheet" href="assets/css/bootstrap.min.css">  
 <link rel="stylesheet" href="assets/css/owl.carousel.min.css">  
-<!-- CSS: Html Designer -->
-<link rel="stylesheet" href="css/variables.css">  
+<!-- CSS: Html Designer --> 
 <link rel="stylesheet" href="css/style.css">  
 
  <style>
